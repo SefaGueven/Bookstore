e17ff073895514521a2432088ee6af813bbb09d0
Sefa Güven <sefagueven003@gmail.com>
2026-04-19 17:18:37 +0200

    assets und db hinzugefügt.

---
+++ script.js
@@ -0,0 +1,14 @@
+function init() {
+    showBookLibrary();
+    
+}
+function showBookLibrary() {
+    let libraryContainer = document.getElementById("library_container");
+    libraryContainer.innerHTML =""; //Inhalt leeren
+    for (let indexBook = 0; indexBook < book.length; indexBook++) {
+        const book = book[indexBook];
+        // Ein Buch entspricht ein Array aus der liste.
+        
+    }
+    
+}
